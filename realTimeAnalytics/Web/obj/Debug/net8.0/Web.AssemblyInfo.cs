7dd444f3b3cb351a9a23fb56416bedcfa67294a7
Oleh Prostakov <prostakov.oleh@gmail.com>
2024-04-01 19:03:21 +0300

    Remove redundant files from analytics

--- realTimeAnalytics/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ realTimeAnalytics/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9046ba4a47b27e236d4864b32bb76d9a5c594c3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d2b3075f236dd7f7c88d8076f8b3274fe238f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
